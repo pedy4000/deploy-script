d8753ee991f571e44d2ac9cb5045a1486924e916
pedram <pedy4000@gmail.com>
2023-01-31 13:18:03 +0330

    Update script.sh

--- script.sh
+++ script.sh
@@ -97,6 +97,7 @@ setup_colors
 if [ -d "$service_path/$service_name" ] 
 then
     cd $service_path/$service_name
+    git checkout $service_branch_name
     git pull $service_remote_name $service_branch_name --force
 else
     die "$service_path/$service_name doesn't exist"
