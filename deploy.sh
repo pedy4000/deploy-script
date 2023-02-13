94fc188aa7591a6dcec56c9615d835fb073a6e75
pedram <pedy4000@gmail.com>
2023-02-13 21:57:44 +0330

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -150,4 +150,6 @@ cd $service_path/$service_name/secrets/$service_branch_name
 sudo awk '{filename=$1; print $2 > filename; close(filename)}' $password
 
 sudo docker stack deploy --with-registry-auth --compose-file $service_path/$service_name/docker-compose.yml --compose-file $service_path/$service_name/docker-compose.$service_branch_name.yml $service_name-$service_branch_name
+echo $?;
+
 sudo bash /home/$user/deploy-script/docker-stack-wait.sh $service_name-$service_branch_name
